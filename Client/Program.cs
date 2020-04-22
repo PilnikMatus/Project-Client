d88e20a4c1e5fe98a80f656c2e60670cd0411c61
danstepanek <61269423+danstepanek@users.noreply.github.com>
2020-04-22 23:18:18 +0200

    postup

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,20 +6,33 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using ClientDemon.Tables;
+using System.Threading;
 
 namespace ClientDemon
 {
-class Program
- {
+    class Program
+    {
         static void Main(string[] args)
         {
+
+            Timer timer = new Timer(Tick, null, 0, 5000);
+
+            while (true)
+            {
+                //check jestli už je tento v PC v databazi, pokud ano, dále; pokud ne, přidá se.
+                //check jestli je aktivní,
+                //pokud ano, zjistí, data o zálohování
+            }
+        }
+        public static void Tick(object O)
+        {
+            Console.WriteLine("Update");
+
             HttpRequests httpRequests = new HttpRequests();
 
             httpRequests.Post();
-
-            Console.ReadLine();
         }
-        
+
     }
 
 }
